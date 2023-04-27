2d66b98dfbea22dbb9c3dcb62eabc76d7da75ce9
Aloysius <aloysius.joseph13@gmail.com>
2023-04-27 14:37:40 +0530

    init

---
+++ Day1 Brand Name Generator.py
@@ -0,0 +1,9 @@
+#Create a greeting for your program
+print("Welcome to the Band Name Genetator")
+#Ask the user for the city that you grew up in.
+city = input ("What's the name of the city you grew up in?\n")
+#Ask the user the name of a his pet.
+pet_name = input ("What is the name of your pet?\n")
+#Combine the name of their city and pet and show them their band name
+print("Your band name could be "+ city + " " + pet_name)
+# Make sure the input cursor shows a new line:
