5267a0c90fcc747af73c1d2e3f1adf7be28ad9db
Aloysius <aloysius.joseph13@gmail.com>
2023-04-27 14:30:18 +0530

    init

---
+++ Day1.1.py
@@ -0,0 +1,7 @@
+print("Hello World!\nHello World!\nHello World!\n")
+print("Hello" +" " + "Aloysius")
+input("what is your name?\n")
+print("Hello " + input("what is your name? "))
+print(len(input("what is your name? ")))
+
+
